ce8c355dd340f443722765e9035b398733a3bdc6
ocheretnyi <ocheretnyi.spb@gmail.com>
2022-11-21 19:08:39 +0300

    add script slider

---
+++ #src/js/slider.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const btnPrevSlide = document.querySelector(".slider__btn--prev");
+const btnNextSlide = document.querySelector(".slider__btn--next");
+const listSlides = document.querySelectorAll(".slider__item");
+
+btnNextSlide.addEventListener("click", () => {
+
+})

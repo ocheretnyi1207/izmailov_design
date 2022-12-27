7ef3a075472d54ba2f84c9080402b711b7cc622b
ocheretnyi <ocheretnyi.spb@gmail.com>
2022-12-27 19:35:30 +0300

    add event open menu catalog

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,5 +1,9 @@
+const ESCKEYCODE = 32;
+
 const btnOpenMenu = document.querySelector(".page-header__btn");
 const navListHeader = document.querySelector(".nav-list--header");
+const btnOpenMenuCatalog = document.querySelector(".catalog-navigation__btn");
+const catalogMenu = document.querySelector(".catalog-list");
 
 btnOpenMenu.addEventListener("click", () => {
   navListHeader.classList.toggle("nav-list--disable")
@@ -7,7 +11,20 @@ btnOpenMenu.addEventListener("click", () => {
 
 btnOpenMenu.addEventListener("keydown", (evt) => {
   evt.preventDefault()
-  if (evt.keyCode === 32) {
+  if (evt.keyCode === ESCKEYCODE) {
     navListHeader.classList.toggle("nav-list--disable")
   }
 })
+
+btnOpenMenuCatalog.addEventListener("click", () => {
+  catalogMenu.classList.toggle("catalog-list--active")
+})
+
+btnOpenMenuCatalog.addEventListener("keydown", (evt) => {
+  evt.preventDefault()
+  if (evt.keyCode === ESCKEYCODE) {
+    catalogMenu.classList.toggle("catalog-list--active")
+  }
+})
+
+

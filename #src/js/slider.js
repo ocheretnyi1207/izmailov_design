ff3529295e678b05cafe4f7350a45c0655fc09f3
ocheretnyi <ocheretnyi.spb@gmail.com>
2023-01-18 01:31:06 +0300

    add code slider

--- #src/js/slider.js
+++ #src/js/slider.js
@@ -1,9 +1,41 @@
-"use strict"
+let index = 0;
 
-const btnPrevSlide = document.querySelector(".slider__btn--prev");
-const btnNextSlide = document.querySelector(".slider__btn--next");
-const listSlides = document.querySelectorAll(".slider__item");
+document.addEventListener("click", (evt) => {
+  const activeSlide = (n) => {
+    slides.forEach((element) => {
+      return element.classList.remove("slider__item--active")
+    })
+    slides[n].classList.add("slider__item--active")
+  }
 
-btnNextSlide.addEventListener("click", () => {
+  const prevSlide = () => {
+    if (index === 0) {
+      index = slides.length -1;
+      activeSlide(index);
+    } else {
+      index --;
+      activeSlide(index);
+    }
+  }
 
-})
+  const nextSLide = () => {
+    if (index === slides.length - 1) {
+      index = 0;
+      activeSlide(index)
+    } else {
+      index ++;
+      activeSlide(index);
+    }
+  }
+
+  const slider = evt.target.parentNode;
+  const slides = slider.querySelectorAll(".slider__item");
+
+  if(evt.target.classList.contains("slider__btn--next")) {
+    nextSLide();
+  }
+
+  if (evt.target.classList.contains("slider__btn--prev")) {
+    prevSlide();
+  }
+});

c95df46411822f034f84f14b814b624f9917ff27
ocheretnyi <ocheretnyi.spb@gmail.com>
2023-01-25 01:32:26 +0300

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const WAY = {
+    INDEX: "./#src/js/pages/index.js",
+    CATALOG: "./#src/js/pages/catalog.js"
+}
+
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: WAY.INDEX,
+        catalog: WAY.CATALOG
+    },
+    output: {
+        path: path.resolve(__dirname, 'js'),
+        filename: "[name].bundle.js"
+    },
+    mode: "production"
+}

34bb469f02e6586093785fe6b535511935e6a9e4
ocheretnyi <ocheretnyi.spb@gmail.com>
2023-01-25 15:04:47 +0300

    add constants

--- #src/js/constants.js
+++ #src/js/constants.js
@@ -1,7 +1,8 @@
-const ESCKEYCODE = 32;
+const ESCKEYCODE = 27;
+const SPACEKEYCODE = 32;
 const HEIGHT_SHOW_BTNUP = 800;
 const DOT_CLASS = "gallery-slider__dotted";
 const DOT_ACTIVE_CLASS = "gallery-slider__dotted--active";
 const ITEM_ACTIVE_CLASS = "gallery-slider__item--active";
 
-export { ESCKEYCODE, HEIGHT_SHOW_BTNUP, DOT_CLASS, DOT_ACTIVE_CLASS, ITEM_ACTIVE_CLASS };
+export { ESCKEYCODE, SPACEKEYCODE, HEIGHT_SHOW_BTNUP, DOT_CLASS, DOT_ACTIVE_CLASS, ITEM_ACTIVE_CLASS };
